e78f29260c53ccfdc0d5a785095be12cb542d962
Andreas <andreas.basso@std.heh.be>
2020-11-26 16:39:11 +0100

    delete tout

--- ProjectEPM-master/adapters/EPM-persistence/src/main/java/be/heh/epm/adapter/web/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package be.heh.epm.adapter.web;
-
-class EmployeeNotFoundException extends RuntimeException {
-    EmployeeNotFoundException(int id) {
-        super("Could not find employee " + id);
-    }
-}
--- ProjectEPM-master/adapters/EPM-persistence/src/main/java/be/heh/epm/adapter/web/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package be.heh.epm.adapter.web;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-interface EmployeeRepository extends JpaRepository<EmployeeSalariedValidating, Integer> {
-
-}
