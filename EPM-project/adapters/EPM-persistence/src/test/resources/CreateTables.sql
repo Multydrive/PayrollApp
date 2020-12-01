f30e7a261155e9b6f678b516bca27df2e04d885b
Adrien <doudou.delau@hotmail.fr>
2020-12-01 11:54:42 +0100

    modification base de données heroku

--- EPM-project/adapters/EPM-persistence/src/test/resources/CreateTables.sql
+++ EPM-project/adapters/EPM-persistence/src/test/resources/CreateTables.sql
@@ -1,18 +1,25 @@
-CREATE TABLE IF NOT EXISTS Employee (
-                                        empId SERIAL,
-                                        name VARCHAR(45) NOT NULL,
-                                        address VARCHAR(45) NOT NULL,
-                                        mail VARCHAR(45) NOT NULL,
-                                        paymentClassificationType VARCHAR(45) NOT NULL,
-                                        paymentMethodType VARCHAR(45) NOT NULL,
-                                        paymentScheduleType VARCHAR(45) NOT NULL,
-
-                                        primary key (empId)
+CREATE TABLE IF NOT EXISTS public."EMPLOYEE"
+(
+    "ID" serial,
+    "PAYMENT_METHOD" character varying,
+    "PAYMENT_SCHEDULE" character varying,
+    "EMPLOYEE_CLASSIFICATION" character varying,
+    "SURNAME" character varying,
+    "FIRSTNAME" character varying,
+    "ADDRESS" character varying,
+    "MAIL" character varying,
+    PRIMARY KEY ("ID")
 );
 
-CREATE TABLE IF NOT EXISTS SalariedClassification (
-                                      mount double precision,
-                                      empId int,
-                                      primary key (empId),
-                                      foreign key(empId) references Employee(empId)
+CREATE TABLE IF NOT EXISTS public."SALARIEDCLASSIFICATION"
+(
+    "ID" serial,
+    "EMPLOYEE_ID" integer,
+    "SALARY" integer,
+    PRIMARY KEY ("ID"),
+    CONSTRAINT "SALARIED_EMPLOYEE" FOREIGN KEY ("EMPLOYEE_ID")
+        REFERENCES public."EMPLOYEE" ("ID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 );
